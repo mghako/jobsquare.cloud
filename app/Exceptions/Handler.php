27886c83d86f9a48b42dc93b306dd4bedafcb62f
htet aung ko <htetaungko@yahoo.com>
2022-05-26 23:47:42 +0630

    api response error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -32,10 +36,59 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
+    public function register() {
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Record not found.'
+                ], 404);
+            }
         });
+
     }
+
+    public function render($request, Throwable $exception) {
+        if ($request->is('api/*')) {
+            return $this->handleException($request, $exception);
+        }
+    
+        return parent::render($request, $exception);
+
+    }
+
+  
+    public function handleException($request, Exception $exception) {
+
+        if ($exception instanceof MethodNotAllowedException) {
+            return response()->json([
+                'message' => 'The specified method for the request is invalid'
+            ], 405);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'message' => 'Not Found!'
+            ], 404);
+        }
+
+        if ($exception instanceof HttpException) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ], $exception->getStatusCode());
+        }
+
+        if (config('app.debug')) {
+            return parent::render($request, $exception);            
+        }
+
+        return response()->json([
+            'message' => 'Unexpected Exception. Try later!'
+        ], 500);
+
+    }
+
 }
